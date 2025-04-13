061646da2cf9d2eb99e6d71a6c09aadff5967955
med-b <mohmed.bourkha@gmail.com>
2025-04-13 20:10:52 +0200

    decoarting with cach module : init

---
+++ refactoring-embellishment-to-decorator-master/java/src/main/java/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    public final String title;
+    public final String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
