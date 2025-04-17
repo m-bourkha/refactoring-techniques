e58f0dcc938a24f53d85c6ce34f8d44d15df1ccc
med-b <mohmed.bourkha@gmail.com>
2025-04-18 01:18:50 +0200

    resolve rebase conflict 2

---
+++ refactoring-conditional-to-polymorphism-master/java/src/main/java/com/bourkha/refactoring/Direction.java
@@ -0,0 +1,25 @@
+package com.bourkha.refactoring;
+
+public enum Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST;
+
+    public Direction left() {
+        int left = ordinal() - 1;
+        if (left < 0) {
+            left = values().length - 1;
+        }
+        return Direction.values()[left];
+    }
+
+    public Direction right() {
+        int right = ordinal() + 1;
+        if (right >= values().length) {
+            right = 0;
+        }
+        return Direction.values()[right];
+    }
+
+}
---
+++ refactoring-conditional-to-polymorphism-master/java/src/main/java/com/bourkha/refactoring/ReportingModule.java
@@ -0,0 +1,8 @@
+package com.bourkha.refactoring;
+
+public interface ReportingModule {
+
+    void reportPosition(Point point);
+
+    void reportDirection(Direction initialDirection);
+}
